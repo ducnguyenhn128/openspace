355814619ae48c90fe3f3fd9a04776ad9012adea
ducnguyenhn128 <ducnguyen.dautunhanha@gmail.com>
2023-05-22 23:51:49 +0700

    cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const tagRouter = require('./src/controllers/tagRouter');
 
 // CORS
 app.use(cors({
-    origin: ['http://localhost:3000' ,'/\.netlify\.app$/}' ] ,
+    origin: true ,
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
     credentials: true, // Allow credentials (e.g., cookies)
