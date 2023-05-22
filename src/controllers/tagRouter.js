6a97fa5473517674077f33921542ead0acb20c01
ducnguyenhn128 <ducnguyen.dautunhanha@gmail.com>
2023-05-22 23:12:22 +0700

    tag

--- src/controllers/tagRouter.js
+++ src/controllers/tagRouter.js
@@ -2,6 +2,11 @@ const express = require('express')
 const postCRUD = require('../model/post');
 
 const tagRouter = express.Router();
+
+tagRouter.get('/', (req, res) => {
+    res.send('OK')
+})
+
 tagRouter.get('/:tag', postCRUD.getPostByTag)
 
 module.exports = tagRouter
